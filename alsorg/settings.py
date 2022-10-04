f26b21effcceb8c2efed970fc8301054e795e51e
shivaconsole <72904837+shivaconsole@users.noreply.github.com>
2022-10-04 11:09:05 -0700

    Update settings.py

--- alsorg/settings.py
+++ alsorg/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -142,3 +142,5 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 
 AUTH_USER_MODEL = 'leads.User'
 LOGIN_REDIRECT_URL = '/leads'
+
+django_heroku.settings(locals())
