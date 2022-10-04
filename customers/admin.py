b08e9151579f40826231411f5899de66734b4b7b
shivaconsole <72904837+shivaconsole@users.noreply.github.com>
2022-10-04 11:45:36 +0530

    Delete customers directory

--- customers/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Customer
-from .models import BelongstoBusinnesnit
-# Register your models here.
-
-admin.site.register(Customer)
-admin.site.register(BelongstoBusinnesnit)
