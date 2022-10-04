c060686d59854c43f665b24736df3d2dbb6d4252
shivaconsole <72904837+shivaconsole@users.noreply.github.com>
2022-10-04 15:49:43 +0530

    Delete leads directory

--- leads/constants.py
+++
@@ -1,9 +0,0 @@
-# lead_name = models.CharField(max_length=20)
-CHOICES_SAL = (('Mr.', 'MR.'), ('Mrs.', 'MRS.'), ('Shri', 'SHRI'), ('Shrimati', 'SHRIMATI'))
-CHOICES_BU = (('FACTORY_UNIT', 'Factory-Unit'), ('CALLISTO-ELEMENTS', 'Callisto-Elements'))
-LEAD_STATE = (('OPEN', 'Open'), ('QUALIFIED', 'Qualified'), ('LOST', 'Lost'))
-LEAD_CATEGORY = (('CORPORATE', 'Corporate'), ('PARTNER', 'Partner'), ('RETAIL', 'Retail'))
-PRODUCT_GROUP = (
-('HOME AND KITCHEN', 'Home and Kitchen'), ('INTERIOR AND TURKNEY SOLUTIONS', 'Interior and Turkney Solutions'),
-('OFFICE FURNITURE', 'Office Furniture'))
-
